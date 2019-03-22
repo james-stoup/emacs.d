eb6eb34efb9181aa462269f1022f1400a3e59a6d
James Stoup <jrstoup@gmail.com>
2019-03-22 14:14:21 -0400

    Big cleanup of all my emacs deps
    Lots of new stuff and lots of stuff getting thrown out. Moving from
    Go to Java & Python will do that to you.

---
+++ .emacs.d/elpa/pyflakes-1.0/pyflakes-autoloads.el
@@ -0,0 +1,22 @@
+;;; pyflakes-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "pyflakes" "pyflakes.el" (23701 3504 741179
+;;;;;;  852000))
+;;; Generated autoloads from pyflakes.el
+
+(autoload 'pyflakes "pyflakes" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pyflakes-autoloads.el ends here
---
+++ .emacs.d/elpa/pyimpsort-20160130.453/pyimpsort-autoloads.el
@@ -0,0 +1,32 @@
+;;; pyimpsort-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "pyimpsort" "pyimpsort.el" (23701 4896 818579
+;;;;;;  174000))
+;;; Generated autoloads from pyimpsort.el
+
+(autoload 'pyimpsort-region "pyimpsort" "\
+Sort python imports from region BEGIN to END points.
+
+\(fn BEGIN END)" t nil)
+
+(autoload 'pyimpsort-buffer "pyimpsort" "\
+Sort python imports from current buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("pyimpsort-pkg.el") (23701 4896 735579
+;;;;;;  989000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pyimpsort-autoloads.el ends here
